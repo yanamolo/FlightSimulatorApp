d4fdceade16fe0a59dc7a6e7eaeb1166d1b0d513
Yana Molodetsky <59420437+yanamolo@users.noreply.github.com>
2020-03-18 11:53:35 +0200

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace firstproject
+{
+    class Client : IClient
+    {
+        const int BUFFER_SIZE = 1024;
+        private Socket sender;
+        public void Connect(string ip, int port)
+        {
+            try
+            {
+
+                // Establish the remote endpoint for the socket.
+                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddr = IPAddress.Parse(ip);
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, port);
+
+                // Creation TCP/IP Socket
+                this.sender = new Socket(ipAddr.AddressFamily,
+                           SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+
+                    // Connect Socket to the remote  
+                    // endpoint using method Connect() 
+                    sender.Connect(localEndPoint);
+
+                    // We print EndPoint information  
+                    // that we are connected 
+                    Console.WriteLine("Socket connected to -> {0} ",
+                                  sender.RemoteEndPoint.ToString());
+
+                }
+                catch (ArgumentNullException ane)
+                {
+
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+
+                catch (SocketException se)
+                {
+
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public void Disconnect()
+        {
+            this.sender.Close();
+        }
+
+        public string Read()
+        {
+            byte[] messageReceived = new byte[BUFFER_SIZE];
+            int byteRecv = sender.Receive(messageReceived);
+            Console.WriteLine("Message from Server -> {0}",
+                  Encoding.ASCII.GetString(messageReceived,
+                                             0, byteRecv));
+            return Encoding.ASCII.GetString(messageReceived, 0, byteRecv);
+        }
+
+        public void Write(string command)
+        {
+            byte[] messageSent = Encoding.ASCII.GetBytes(command);
+            _ = sender.Send(messageSent); //int bytes
+        }
+    }
+}
---
+++ IClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstproject
+{
+    interface IClient
+    {
+        void Connect(string ip, int port);
+        void Write(string command);
+        string Read();
+        void Disconnect();
+    }
+}
