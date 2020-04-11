8664f6b2ae56f58db073ffb5cec12e90fdba99da
OritFine <orityf545@gmail.com>
2020-04-11 23:28:37 +0300

    Added referece for ConfigurationManager, also App.xaml.cs merge fix

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,12 @@
-namespace FlightSimulatorApp
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FlightSimulator
 {
     /// <summary>
     /// Interaction logic for App.xaml
